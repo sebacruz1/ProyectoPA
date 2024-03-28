3048faf8670386768a696981369665f0944d6307
sebacruz1 <sebastiancruzpomar@gmail.com>
2024-03-27 21:02:10 -0300

    cambios generales

--- src/app/Curso.java
+++ src/app/Curso.java
@@ -1,47 +1,70 @@
 package app;
 
-import app.Alumno;
-import java.util.LinkedList;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Curso {
     private String nombre;
-    private LinkedList<Alumno> alumnos;
-    private int cantidadAlumnos;
+    private ArrayList<Alumno> alumnos;
+    private int totalAlumnos;
+    private Map<LocalDate, RegistroAsistencia> asistenciasPorFecha;
+    
 
        
-    public Curso(String nombre, String curso, LinkedList<Alumno> alumnos) {
+    public Curso(String nombre, String curso, ArrayList<Alumno> alumnos, 
+            int totalAlumnos, Map<LocalDate, RegistroAsistencia> asistenciaPorFecha) {
         this.nombre = nombre;
         this.alumnos = alumnos;
+        this.totalAlumnos = totalAlumnos;
+        this.asistenciasPorFecha = asistenciaPorFecha;
     }
 
         
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-  
+ 
     public String getNombre() {
         return nombre;
     }
         
-    public LinkedList<Alumno> getAlumnos() {
+    public ArrayList<Alumno> getAlumnos() {
         return alumnos;
     }
 
-    public void setAlumnos(LinkedList<Alumno> alumnos) {
+    public void setAlumnos(ArrayList<Alumno> alumnos) {
         this.alumnos = alumnos;
     }
     
-    public int getCantidadAlumnos() {
-        return cantidadAlumnos;
+    public int getTotalAlumnos() {
+        return totalAlumnos;
     }
 
-    public void setCantidadAlumnos(int cantidadAlumnos) {
-        this.cantidadAlumnos = cantidadAlumnos;
+    public void setTotalAlumnos(int alumnos) {
+        this.totalAlumnos = alumnos;
     }
+
+    public void registrarAsistencia(LocalDate fecha, int cantidadAlumnosPresentes) {
+        if (!asistenciasPorFecha.containsKey(fecha)) {
+            asistenciasPorFecha.put(fecha, new RegistroAsistencia(fecha, totalAlumnos));
+        }
+        RegistroAsistencia registro = asistenciasPorFecha.get(fecha);
+        registro.registrarAsistencia(cantidadAlumnosPresentes);
+    }
+     
+    public Map<LocalDate, RegistroAsistencia> getAsistenciasPorFecha() {
+        return asistenciasPorFecha;
+    }
+    
+    public void setAsistenciasPorFecha(Map<LocalDate, RegistroAsistencia> asistenciasPorFecha) {
+        this.asistenciasPorFecha = asistenciasPorFecha;
+    }
+
+
 
    
     
     
    
-
 }
